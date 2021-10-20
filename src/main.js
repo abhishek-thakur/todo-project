ef30f3bf970971179cf9eec220a8d3461a373889
Abhishek Thakur <th.abhishek12@gmail.com>
2021-10-20 15:23:35 +0530

    modal been created for update task

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/TaskComponent.vue
+++ src/views/TaskComponent.vue
@@ -1,43 +1,80 @@
 <template>
-    <tr>
-        <td>
-            <span :class="{'finished': task.completed == true}">{{task.name}}</span>
-        </td>
-        <td>
-            <div class="pointer" @click="isCompletedTC(task)">
-                <span :class="{'fas fa-check':task.completed, 'fas fa-hourglass-half':!task.completed }"></span>
-            </div>
-        </td>
-        <td>
-            <div @click="editTaskTC(task)" class="pointer">
-                <span class="fas fa-pen-fancy"></span>
-            </div>
-        </td>
-        <td>
-            <div @click="deleteTaskTC(task)" class="pointer">
-                <span class="fas fa-trash-alt"></span>
-            </div>
-        </td>
-    </tr>
-
+  <tr>
+    <td>
+      <span :class="{ finished: task.completed == true }">{{ task.name }}</span>
+    </td>
+    <td>
+      <div class="pointer" @click="isCompletedTC(task)">
+        <span
+          :class="{
+            'fas fa-check': task.completed,
+            'fas fa-hourglass-half': !task.completed,
+          }"
+        ></span>
+      </div>
+    </td>
+    <td>
+      <div class="pointer">
+        <b-button v-b-modal.modal-prevent-closing><span class="fas fa-pen-fancy"></span></b-button>
+      </div>
+      <b-modal id="modal-prevent-closing"
+        ref="modal"
+        title="Update the Task"
+        @ok="editTaskTC(task.id)" centered
+      >
+        <input type="text" 
+          v-model="upTask"
+          class="form-control"
+        />
+      </b-modal>
+    </td>
+    <td>
+      <div @click="deleteTaskTC(task)" class="pointer">
+        <span class="fas fa-trash-alt"></span>
+      </div>
+    </td>
+  </tr>
 </template>
 <script>
 export default {
-    name: 'TaskComponent',
-    props: ['task'],
-    methods:{
-        editTaskTC(task){
-            //alert("editing")
-            this.$emit('editTask', task.id);
-        },
-        deleteTaskTC(task){
-            //alert("deleting");
-            this.$emit('deleteTask', task.id);
-        },
-        isCompletedTC(task){
-            //alert("completed");
-            this.$emit('isCompleted', task.id);
+  name: "TaskComponent",
+  data() {
+    return {
+      upTask:this.task.name
+    };
+  },
+  props: ["task"],
+  methods: {
+      // handleOk(bvModalEvt) {
+      //   bvModalEvt.preventDefault()
+      //   this.handleSubmit()
+      // },
+      // handleSubmit() {
+      //   if (this.upTask.length == 0) {
+      //     return
+      //   }
+      //   this.$nextTick(() => {
+      //     this.$bvModal.hide('modal-prevent-closing')
+      //   })
+      // },
+    editTaskTC(id) {
+      //alert("editing")
+      if (this.upTask.length == 0) {
+          return
         }
-    }
-}
+        this.$emit("editTask",id);
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
+    },
+    deleteTaskTC(task) {
+      //alert("deleting");
+      this.$emit("deleteTask", task.id);
+    },
+    isCompletedTC(task) {
+      //alert("completed");
+      this.$emit("isCompleted", task.id);
+    },
+  },
+};
 </script>
